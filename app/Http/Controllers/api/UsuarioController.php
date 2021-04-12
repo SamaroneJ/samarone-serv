a6c0aedcf5f1b7edf3805a0294f973dd55d09530
Samarone <samarone@alunos.utfpr.edu.br>
2021-04-12 11:20:40 -0300

    rotas

---
+++ app/Http/Controllers/api/UsuarioController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Models\usuario;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+        //return 'ok';
+        return usuario::all();
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+        public function store(Request $request)
+    {
+        //
+    }
+
+       public function show($id)
+    {
+        //
+    }
+
+        public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+       public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Models/usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class usuario extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'idusuario',
+        'tipo',
+        'nome',
+        'email',
+        'senha',
+    ];
+}
