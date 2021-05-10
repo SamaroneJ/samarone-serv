b167bea938bdd324a76f7af97db0db940c288c9d
Samarone <samarone@alunos.utfpr.edu.br>
2021-05-10 20:10:21 -0300

    10-05

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;*/
 
 
 Route::apiResource('login','App\Http\Controllers\api\UsuarioController');
+Route::apiResource('logout','App\Http\Controllers\LogoutController');
 
 
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
